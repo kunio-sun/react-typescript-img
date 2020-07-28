f13478dbf8bc0215af00f94dd0e25ae65dca05cf
Tanaka Kuniaki <kuni1100001@gmail.com>
2020-07-28 18:28:46 +0900

    プロジェクト作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const ts: string = "TypeScript";
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>Hello,{ts}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
